268cb4fd4815921ef4273e19b92c0ea8d4797185
Abi Fajri <a.fajri303@gmail.com>
2020-08-14 21:28:50 +0700

    init flappy-bird project

---
+++ [DILo Academy] Flappy Bird - Unity Project/Assets/Scripts/Bird.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Bird : MonoBehaviour
+{
+    //Global Variables
+    [SerializeField] private float upForce = 100;
+    [SerializeField] private bool isDead;
+    [SerializeField] private UnityEvent OnJump, OnDead;
+    [SerializeField] private int score;
+    [SerializeField] private UnityEvent OnAddPoint;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Transform bulletPoint;
+    [SerializeField] private GameObject bullet;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    private Rigidbody2D rigidBody2d;
+
+    //init variable
+    void Start()
+    {
+        //Mendapatkan komponent ketika game baru berjalan
+        rigidBody2d = GetComponent<Rigidbody2D>();
+
+        //Mendapatkan komponen animator pada game object   
+        animator = GetComponent<Animator>();
+    }
+
+    //Update setiap frame  
+    void Update()
+    {
+        //Melakukan pengecekan jika belum mati dan klik kiri pada mouse
+        if (!isDead && Input.GetMouseButtonDown(0))
+        {
+            //Burung meloncat
+            Jump();
+        }
+
+        //Melakukan pengecekan jika belum mati dan klik kiri pada mouse
+        if (!isDead && Input.GetKeyDown("space"))
+        {
+            //Burung meloncat
+            Shoot();
+        }
+    }
+
+    //Fungsi untuk mengecek sudah mati apa belum
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    //Membuat Burung Mati
+    public void Dead()
+    {
+        //Pengecekan jika belum mati dan value OnDead tidak sama dengan Null
+        if (!isDead && OnDead != null)
+        {
+            //Memanggil semua event pada OnDead
+            OnDead.Invoke();
+        }
+
+        //Mengeset variable Dead menjadi True
+        isDead = true;
+
+    }
+
+    void Jump()
+    {
+        //Mengecek rigidbody null atau tidak
+        if (rigidBody2d)
+        {
+            //menghentikan kecepatan burung ketika jatuh
+            rigidBody2d.velocity = Vector2.zero;
+
+            //Menambahkan gaya ke arah sumbu y agar burung meloncat
+            rigidBody2d.AddForce(new Vector2(0, upForce));
+        }
+
+        //Pengecekan Null variable
+        if (OnJump != null)
+        {
+            //Menjalankan semua event OnJump event
+            OnJump.Invoke();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, bulletPoint.position, bullet.transform.rotation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //menghentikan Animasi Burung ketika bersentukan dengan object lain
+        animator.enabled = false;
+    }
+
+    public void AddScore(int value)
+    {
+        //Menambahkan Score value
+        score += value;
+
+        //Pengecekan Null Value
+        if (OnAddPoint != null)
+        {
+            //Memanggil semua event pada OnAddPoint
+            OnAddPoint.Invoke();
+        }
+
+        //Mengubah nilai text pada score text
+        scoreText.text = score.ToString();
+    }
+}
---
+++ [DILo Academy] Flappy Bird - Unity Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(2f, 0f);
+    }
+}
